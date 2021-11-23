a15c9c1323675a45f182a3ae47473ded32598732
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-23 23:00:11 +0100

    bootstrap implemented, etc...

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/Navbar";
+import "../styles/global.scss";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <NavBar />
+      <div className="content">{children}</div>
+      <footer>Made by Princzes Barnabás</footer>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
-    <div>
+    <Layout>
       <h1>heloo world!</h1>
       <img src="https://i.ibb.co/grXGxBw/new-gyor-logo.png" alt="gyor" />
-    </div>
+      <StaticImage src="../images/icon.png" alt="icon" placeholder="blurred" />
+      <Button>asddsfa</Button>
+    </Layout>
   );
 };
 
