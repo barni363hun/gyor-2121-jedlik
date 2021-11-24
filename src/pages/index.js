3af3b13ea1c046224b9762b0bcc087c373e9d475
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-24 09:24:22 +0100

    i18 not works

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { StaticImage } from "gatsby-plugin-image";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
+import { withTranslation } from "react-i18next";
 
 const IndexPage = () => {
   return (
@@ -10,8 +11,8 @@ const IndexPage = () => {
       <h1>heloo world!</h1>
       <img src="https://i.ibb.co/grXGxBw/new-gyor-logo.png" alt="gyor" />
       <StaticImage src="../images/icon.png" alt="icon" placeholder="blurred" />
-      <Button>asddsfa</Button>
-      <a href={process.env.WEB_URL}>this site</a>
+      <Button></Button>
+      <a href={process.env.WEB_URL}>{t("asd")}</a>
     </Layout>
   );
 };
