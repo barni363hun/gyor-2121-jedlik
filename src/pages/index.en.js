dbfd18ec12cba2aed3bb226146d6e3913e8c663f
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-24 09:58:39 +0100

    gatsby theme not works

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -3,16 +3,25 @@ import { StaticImage } from "gatsby-plugin-image";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
-import { withTranslation } from "react-i18next";
+import { useLocalization } from "gatsby-theme-i18n";
+import { LocalesList } from "gatsby-theme-i18n";
 
 const IndexPage = () => {
+  const { locale, config, defaultLang } = useLocalization();
+
   return (
     <Layout>
       <h1>heloo world!</h1>
       <img src="https://i.ibb.co/grXGxBw/new-gyor-logo.png" alt="gyor" />
       <StaticImage src="../images/icon.png" alt="icon" placeholder="blurred" />
-      <Button></Button>
-      <a href={process.env.WEB_URL}>{t("asd")}</a>
+      <Button>magyarrr {locale}</Button>
+      <a href={process.env.WEB_URL}></a>
+      <p>Used lang: {defaultLang}</p>
+      <p>{JSON.stringify(config, null, 2)}</p>
+
+      <p>
+        <LocalesList />
+      </p>
     </Layout>
   );
 };
