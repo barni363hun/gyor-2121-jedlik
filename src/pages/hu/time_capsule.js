041470202e9854987d82bf14e6fac73181daf7b0
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-28 05:49:51 +0100

    iframes added

--- src/pages/hu/time_capsule.js
+++ src/pages/hu/time_capsule.js
@@ -11,7 +11,203 @@ const ArticlePage = () => {
         <Row className="text-center">
           <h2>Időkapszula a jövőnek!</h2>
         </Row>
-        <Row></Row>
+        <Row>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1Hr4hsVYzoN9tVSePUcuBQEjpSNmkz2FH/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1Lp9ilKYSmqt73hXLwt7cVslzvSwedAhY/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1_Jj4x9Oldwe9SaRZRD5Oqcq6kBMwX6DB/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1v-TGnzVmqLL-B3TTeQiL8ehDULLxnS95/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1F8LgIgYxrazbWXAxyu8PQOxT-S-5m-NR/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1zS_YJd1ho74OiLXMqo_rJG_Oi4iGtbAz/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1ktGQanz9RonQzwhgFT2D7YGz9eHm0F-k/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1UqF4fudLAhdbLQ90IHd_ScIkRvi6Z4hT/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1kxGltNlX1fmnb4fJ2a5N9f35PQubmUKF/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1LZalntIHyimvEE5HfqzXGmVU9gEIUE6m/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1PM_fOcikRjcTrmKlMFaysjwq-OMgHqNt/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1CTGvgxn68gq9ov_1ZosAqLJPfa9M5SMp/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1VeUNHM25rT4VBovaYDHdCKBeb8A6btFw/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1dj5CN0DLIRG5VO6reDIfncl4fqLKJsEE/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1YUPmcXHMsOgUISw5q31QnJr8r6KiFqlq/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1tfIsE0ghiIrNxiPN5qRdnJsuineaVR97/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1B4THlCQRpJ3WgxjkftFyJvswe_V0Duzm/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1YVAdyUTiYhZVRz-UlKS8wqY-Qy1p4gRy/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe
+                src="https://drive.google.com/file/d/1oxD-ZBqFYMNnATLlbWFzuupOyU-IAvn2/preview"
+                width="640"
+                height="480"
+                allow="autoplay"
+              ></iframe>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </HuLayout>
   );
